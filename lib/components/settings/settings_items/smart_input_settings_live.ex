7c5b412d6bebac7eaa449d24d36d1b2a414c0072
Mayel de Borniol <git@mayel.space>
2022-07-08 10:57:09 +1200

    Smart input

--- lib/components/settings/settings_items/smart_input_settings_live.ex
+++ lib/components/settings/settings_items/smart_input_settings_live.ex
@@ -1,4 +1,4 @@
-defmodule Bonfire.UI.Me.SettingsViewsLive.SmartInputLive do
+defmodule Bonfire.UI.Me.SettingsViewsLive.SmartInputSettingsLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop scope, :atom, default: nil
