f7c0196c2261fd1dacf1616aaa5819ef76ced386
Mayel de Borniol <git@mayel.space>
2022-07-30 21:28:18 +1200

    Prop

--- lib/components/settings/settings_items/discoverable_live.ex
+++ lib/components/settings/settings_items/discoverable_live.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.UI.Me.SettingsViewsLive.DiscoverableLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop label, :string
+  prop label, :string, default: nil
   prop scope, :atom, default: nil
 
 end
