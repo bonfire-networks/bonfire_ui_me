ca08ca8230bab5f8470932bf2db8e81f92ce079a
Mayel de Borniol <git@mayel.space>
2022-08-23 17:24:25 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/441

--- lib/views/remote_interaction/remote_interaction_live.ex
+++ lib/views/remote_interaction/remote_interaction_live.ex
@@ -1,7 +1,19 @@
 defmodule Bonfire.UI.Me.RemoteInteractionLive do
   use Bonfire.UI.Common.Web, :surface_live_view
+  alias Bonfire.UI.Me.LivePlugs
 
   def mount(params, session, socket) do
+    live_plug params, session, socket, [
+      # LivePlugs.LoadCurrentAccount,
+      LivePlugs.LoadCurrentUser,
+      Bonfire.UI.Common.LivePlugs.StaticChanged,
+      Bonfire.UI.Common.LivePlugs.Csrf,
+      Bonfire.UI.Common.LivePlugs.Locale,
+      &mounted/3
+    ]
+  end
+
+  def mounted(params, session, socket) do
     # debug(session, "session")
     {:ok,
      socket
