9e983986f07ff44bddb8df4adc4df41ae10d8433
Mayel de Borniol <git@mayel.space>
2022-09-26 16:44:23 +1300

    https://github.com/bonfire-networks/bonfire-app/issues/451

--- lib/live_handlers/settings_live_handler.ex
+++ lib/live_handlers/settings_live_handler.ex
@@ -2,6 +2,21 @@ defmodule Bonfire.Me.Settings.LiveHandler do
   use Bonfire.UI.Common.Web, :live_handler
   # import Bonfire.Boundaries.Integration
 
+  def handle_event("put", %{"keys" => keys, "value" => value}, socket) do
+    with {:ok, settings} <-
+           keys
+           |> String.split(":")
+           |> input_to_atoms()
+           |> debug()
+           |> Bonfire.Me.Settings.put(value, socket) do
+      # debug(settings, "done")
+      {:noreply,
+       socket
+       |> maybe_assign_context(settings)
+       |> assign_flash(:info, "Settings saved :-)")}
+    end
+  end
+
   def handle_event("set", attrs, socket) when is_map(attrs) do
     with {:ok, settings} <- Map.drop(attrs, ["_target"]) |> Bonfire.Me.Settings.set(socket) do
       # debug(settings, "done")
