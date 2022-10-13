bc41cc721756705522aae9267e214268d3a1ea55
ivanminutillo <ivanminutillo@outlook.it>
2022-10-13 13:05:16 +0200

    settings

---
+++ lib/components/instance/posts/instance_posts_live.ex
@@ -0,0 +1,18 @@
+defmodule Bonfire.UI.Me.SettingsViewsLive.InstancePostsLive do
+  use Bonfire.UI.Common.Web, :stateful_component
+
+  prop selected_tab, :string
+
+  def update(assigns, socket) do
+    current_user = current_user(assigns)
+    tab = e(assigns, :selected_tab, nil)
+
+    {:ok,
+     assign(
+       socket,
+       selected_tab: tab,
+       current_user: current_user,
+       pages: Bonfire.Pages.list_paginated()
+     )}
+  end
+end
