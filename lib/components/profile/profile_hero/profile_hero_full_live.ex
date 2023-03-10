4add968e5997687ecb718cde08e931556f916b80
Mayel de Borniol <git@mayel.space>
2023-03-10 17:24:25 +1300

    p

--- lib/components/profile/profile_hero/profile_hero_full_live.ex
+++ lib/components/profile/profile_hero/profile_hero_full_live.ex
@@ -7,7 +7,7 @@ defmodule Bonfire.UI.Me.ProfileHeroFullLive do
   prop follows_me, :boolean, default: false
   prop selected_tab, :string
   prop transparent_header, :boolean, default: false
-  prop showing_within, :any, default: :profile
+  prop showing_within, :atom, default: :profile
   def display_url("https://" <> url), do: url
   def display_url("http://" <> url), do: url
   def display_url(url), do: url
