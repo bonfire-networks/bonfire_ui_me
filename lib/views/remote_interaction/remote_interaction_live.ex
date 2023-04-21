6caf48c3e01344a500eeb5258f0e5957a082d980
Mayel de Borniol <git@mayel.space>
2023-04-22 11:06:32 +1200

    f

--- lib/views/remote_interaction/remote_interaction_live.ex
+++ lib/views/remote_interaction/remote_interaction_live.ex
@@ -9,8 +9,14 @@ defmodule Bonfire.UI.Me.RemoteInteractionLive do
      socket
      |> assign(:canonical_url, e(params, "url", nil) || e(session, "url", nil))
      |> assign(:name, e(params, "name", nil) || e(session, "name", nil))
-     |> assign(:page, e(params, "page", "Remote interaction") || e(session, "page", "Remote interaction"))
-     |> assign(:page_title, e(params, "page", "Remote interaction") || e(session, "page", "Remote interaction"))
+     |> assign(
+       :page,
+       e(params, "page", "Remote interaction") || e(session, "page", "Remote interaction")
+     )
+     |> assign(
+       :page_title,
+       e(params, "page", "Remote interaction") || e(session, "page", "Remote interaction")
+     )
      |> assign(
        :interaction_type,
        e(params, "type", nil) || e(session, "type", nil) || l("follow")
