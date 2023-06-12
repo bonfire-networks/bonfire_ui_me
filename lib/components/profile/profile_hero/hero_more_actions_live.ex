ac2606cc67ae6ee54eac9db599e0b0cd22b039a5
Mayel de Borniol <git@mayel.space>
2023-06-12 10:12:36 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/568

--- lib/components/profile/profile_hero/hero_more_actions_live.ex
+++ lib/components/profile/profile_hero/hero_more_actions_live.ex
@@ -4,9 +4,22 @@ defmodule Bonfire.UI.Me.HeroMoreActionsLive do
 
   prop user, :map
   prop character_type, :atom, default: :user
+  prop boundary_preset, :any, default: nil
   prop silenced_instance_wide?, :boolean, default: false
   prop ghosted_instance_wide?, :boolean, default: false
   prop ghosted?, :boolean, default: false
   prop silenced?, :boolean, default: false
-  prop boundary_preset, :any, default: nil
+
+  defp set_clone_context({_, o}) do
+    [{:clone_context, o}]
+  end
+
+  defp set_clone_context(%{id: id}) do
+    [{:clone_context, id}]
+  end
+
+  defp set_clone_context(other) do
+    warn(other, "cannot clone_context, expected a tuple or a map with an ID")
+    []
+  end
 end
