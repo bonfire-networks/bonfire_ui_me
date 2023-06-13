54c368861646b655a00c3e7b75e27472642f6790
ivanminutillo <ivanminutillo@outlook.it>
2023-06-13 15:48:28 +0200

    ui

--- lib/components/profile/profile_hero/profile_info_live.ex
+++ lib/components/profile/profile_hero/profile_info_live.ex
@@ -8,7 +8,7 @@ defmodule Bonfire.UI.Me.ProfileInfoLive do
   prop ghosted_instance_wide?, :boolean, default: false
   prop ghosted?, :boolean, default: false
   prop silenced?, :boolean, default: false
-  prop boundary_preset, :any, default: nil
+  # prop boundary_preset, :any, default: nil
 
   def display_url("https://" <> url), do: url
   def display_url("http://" <> url), do: url
