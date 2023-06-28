cbdbc60a7c9e309176955900a1103c53325fca56
ivanminutillo <ivanminutillo@outlook.it>
2023-06-28 12:12:24 +0200

    compliance

---
+++ lib/components/settings/import_export/export_live.ex
@@ -0,0 +1,5 @@
+defmodule Bonfire.UI.Me.SettingsViewsLive.ExportLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop selected_tab, :string
+end
---
+++ lib/components/settings/import_export/import_live.ex
@@ -0,0 +1,5 @@
+defmodule Bonfire.UI.Me.SettingsViewsLive.ImportLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop selected_tab, :string
+end
