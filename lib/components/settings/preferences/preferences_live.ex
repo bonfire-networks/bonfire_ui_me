9f4b2e8a36df1c5a52d3b6327e56bd24778b6c99
Mayel de Borniol <git@mayel.space>
2023-08-26 13:56:26 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/685

--- lib/components/settings/preferences/preferences_live.ex
+++ lib/components/settings/preferences/preferences_live.ex
@@ -3,4 +3,17 @@ defmodule Bonfire.UI.Me.SettingsViewsLive.PreferencesLive do
 
   prop selected_tab, :string
   prop scope, :atom, default: :user
+
+  def render(assigns) do
+    scoped =
+      case assigns[:scope] do
+        :account -> current_account(assigns[:__context__])
+        :instance -> :instance
+        _ -> current_user(assigns[:__context__])
+      end
+
+    assigns
+    |> assign(scoped: scoped)
+    |> render_sface()
+  end
 end
