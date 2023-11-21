e1424aa84a4e57bcc251a113875427ffafe43da5
Mayel de Borniol <git@mayel.space>
2023-11-21 00:30:03 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/751

---
+++ lib/components/profile/user_preview_live.ex
@@ -0,0 +1,11 @@
+defmodule Bonfire.UI.Me.UserPreviewLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+  import Bonfire.Common.Utils
+  # import Bonfire.Common.Media
+
+  prop user, :map
+  prop path_prefix, :string, default: "/@"
+  prop go, :string, default: nil
+
+  slot actions, required: false
+end
