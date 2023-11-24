d4ec2853315d9cc01900cd5a4ebc6e389381962d
ivanminutillo <ivanminutillo@outlook.it>
2023-11-24 14:53:02 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/679

--- test/views/settings_test.exs
+++ test/views/settings_test.exs
@@ -2,6 +2,7 @@ defmodule Bonfire.UI.Me.SettingsTest do
   use Bonfire.UI.Me.ConnCase, async: false
   import Phoenix.LiveViewTest
   use Mneme
+  alias Bonfire.Social.Posts
 
   describe "Appearance" do
     test "As a user I want to select a different theme" do
@@ -92,7 +93,7 @@ defmodule Bonfire.UI.Me.SettingsTest do
 
       # force a refresh
       {:ok, refreshed_view, _html} = live(conn, next)
-      open_browser(refreshed_view)
+      # open_browser(refreshed_view)
 
       auto_assert false <-
                     refreshed_view
@@ -108,7 +109,7 @@ defmodule Bonfire.UI.Me.SettingsTest do
 
       view
       |> element("form[data-scope=set_avatar_shape]")
-      |> render_change(%{"Elixir.Bonfire.UI.Common.AvatarLive" => %{"shape" => "false"}})
+      |> render_change(%{"Elixir.Bonfire.UI.Common.AvatarLive" => %{"shape" => "true"}})
 
       # force a refresh
       {:ok, refreshed_view, _html} = live(conn, next)
@@ -126,8 +127,8 @@ defmodule Bonfire.UI.Me.SettingsTest do
       {:ok, view, _html} = live(conn, next)
 
       view
-      |> element("form[data-scope=set_avatar_shape]")
-      |> render_change(%{"Elixir.Bonfire.UI.Common.AvatarLive" => %{"shape" => "false"}})
+      |> element("form[data-scope=set_hide_avatar]")
+      |> render_change(%{"Elixir.Bonfire.UI.Common.AvatarLive" => %{"hide_avatars" => "true"}})
 
       # force a refresh
       {:ok, refreshed_view, _html} = live(conn, next)
@@ -135,6 +136,171 @@ defmodule Bonfire.UI.Me.SettingsTest do
       auto_assert false <-
                     refreshed_view
                     |> has_element?("div[data-scope=sticky_menu] div[data-scope=avatar]")
+    end
+
+    test "As a user I want to set the animal avatar" do
+      account = fake_account!()
+      alice = fake_user!(account)
+      conn = conn(user: alice, account: account)
+      next = "/settings/user/preferences/appearance"
+      {:ok, view, _html} = live(conn, next)
+
+      auto_assert true <-
+                    view
+                    |> has_element?("svg[data-scope=animal_avatar]")
+
+      view
+      |> element("form[data-scope=set_hide_avatar]")
+      |> render_change(%{"Elixir.Bonfire.UI.Common.AvatarLive" => %{"animal_avatars" => "false"}})
+
+      # force a refresh
+      {:ok, refreshed_view, _html} = live(conn, next)
+
+      auto_assert false <-
+                    refreshed_view
+                    |> has_element?("svg[data-scope=animal_avatar]")
+    end
+
+    test "As a user I want to set the compact layout" do
+      account = fake_account!()
+      alice = fake_user!(account)
+
+      attrs = %{
+        post_content: %{summary: "summary", name: "test post name", html_body: "first post"}
+      }
+
+      assert {:ok, post} =
+               Posts.publish(current_user: alice, post_attrs: attrs, boundary: "public")
+
+      conn = conn(user: alice, account: account)
+      next = "/settings/user/preferences/appearance"
+      {:ok, view, _html} = live(conn, next)
+
+      view
+      |> element("form[data-scope=set_compact_layout]")
+      |> render_change(%{"ui" => %{"compact" => "true"}})
+
+      # force a refresh
+      {:ok, refreshed_view, _html} = live(conn, "/")
+
+      # open_browser(refreshed_view)
+      auto_assert true <-
+                    refreshed_view
+                    |> has_element?("article[data-compact]")
+    end
+
+    test "As a user I want to hide actions on feed" do
+      account = fake_account!()
+      alice = fake_user!(account)
+
+      attrs = %{
+        post_content: %{summary: "summary", name: "test post name", html_body: "first post"}
+      }
+
+      assert {:ok, post} =
+               Posts.publish(current_user: alice, post_attrs: attrs, boundary: "public")
+
+      conn = conn(user: alice, account: account)
+      next = "/settings/user/preferences/appearance"
+      {:ok, view, _html} = live(conn, next)
+
+      view
+      |> element("form[data-scope=set_hide_actions_on_feed]")
+      |> render_change(%{
+        "Elixir.Bonfire.UI.Social.Activity.ActionsLive" => %{
+          "feed" => %{"hide_until_hovered" => "true"}
+        }
+      })
+
+      # force a refresh
+      {:ok, refreshed_view, _html} = live(conn, "/")
+
+      # open_browser(refreshed_view)
+      auto_assert true <-
+                    refreshed_view
+                    |> has_element?("article div[data-id=activity_actions][data-hide]")
+    end
+
+    test "As a user I want to hide actions on discussion" do
+      account = fake_account!()
+      alice = fake_user!(account)
+
+      attrs = %{
+        post_content: %{summary: "summary", name: "test post name", html_body: "first post"}
+      }
+
+      assert {:ok, post} =
+               Posts.publish(current_user: alice, post_attrs: attrs, boundary: "public")
+
+      attrs_reply = %{
+        post_content: %{
+          summary: "summary",
+          name: "name 2",
+          html_body: "<p>reply to first post</p>"
+        },
+        reply_to_id: post.id
+      }
+
+      assert {:ok, post_reply} =
+               Posts.publish(current_user: alice, post_attrs: attrs_reply, boundary: "public")
+
+      conn = conn(user: alice, account: account)
+      next = "/settings/user/preferences/appearance"
+      {:ok, view, _html} = live(conn, next)
+
+      view
+      |> element("form[data-scope=set_hide_actions_on_discussion]")
+      |> render_change(%{
+        "Elixir.Bonfire.UI.Social.Activity.ActionsLive" => %{
+          "thread" => %{"hide_until_hovered" => "true"}
+        }
+      })
+
+      # force a refresh
+      {:ok, refreshed_view, _html} = live(conn, "/post/#{post.id}")
+
+      # open_browser(refreshed_view)
+      auto_assert true <-
+                    refreshed_view
+                    |> has_element?("article div[data-id=activity_actions][data-hide]")
+    end
+  end
+
+  describe "Behaviours" do
+    test "Feed activities" do
+    end
+
+    test "default feed" do
+    end
+
+    test "feed default time limit" do
+    end
+
+    test "feed defaykt sort" do
+    end
+
+    test "how many items to show in feeds and other lists" do
+    end
+
+    test "infinite scrolling" do
+    end
+
+    test "sensitive media" do
+    end
+
+    test "hide media" do
+    end
+
+    test "discussion default layout" do
+    end
+
+    test "discussion default sort" do
+    end
+
+    test "highlight notifications indicator" do
+    end
+
+    test "show reaction counts (likes/boosts)" do
     end
   end
 end
