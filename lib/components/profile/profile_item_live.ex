179e15aef6a2bfaa3bc412cf56a9ff99bc1230a8
ivan <ivanminutillo@outlook.it>
2024-01-15 16:15:18 +0100

    md in profile_item summary

--- lib/components/profile/profile_item_live.ex
+++ lib/components/profile/profile_item_live.ex
@@ -3,7 +3,7 @@ defmodule Bonfire.UI.Me.ProfileItemLive do
 
   prop profile, :any
   prop lg, :boolean, default: false
-  prop wrapper_class, :css_class, default: "flex items-center justify-between"
+  prop wrapper_class, :css_class, default: "flex items-start justify-between"
   prop character, :any
   prop class, :css_class
   prop show_controls, :list, default: [:follow]
