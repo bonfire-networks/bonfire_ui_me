8ddfc72c461e6e874d377339a6a451c7a5ae95de
Mayel de Borniol <git@mayel.space>
2024-02-10 22:42:21 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/847

--- lib/components/profile/links/profile_link_live.ex
+++ lib/components/profile/links/profile_link_live.ex
@@ -23,9 +23,16 @@ defmodule Bonfire.UI.Me.ProfileLinkLive do
     |> render_sface()
   end
 
-  def maybe_icon("youtu" <> _), do: "ri:youtube-fill"
-  def maybe_icon("orcid" <> _), do: "simple-icons:orcid"
   def maybe_icon("git" <> _), do: "mdi:git"
+  def maybe_icon("orcid" <> _), do: "simple-icons:orcid"
+  def maybe_icon("liberapay" <> _), do: "streamline:blood-donate-drop-solid"
+  def maybe_icon("tumblr" <> _), do: "icon-park-solid:tumblr"
+  def maybe_icon("reddit" <> _), do: "teenyicons:reddit-solid"
+  def maybe_icon("stackoverflow" <> _), do: "tabler:brand-stackoverflow"
+  def maybe_icon("youtu" <> _), do: "ri:youtube-fill"
+  def maybe_icon("pinterest" <> _), do: "bxl:pinterest"
+  def maybe_icon("linkedin" <> _), do: "mdi:linkedin"
+  def maybe_icon("twitter" <> _), do: "ph:twitter-logo-thin"
   def maybe_icon("instagram" <> _), do: "ri:instagram-line"
   def maybe_icon(_), do: nil
 end
