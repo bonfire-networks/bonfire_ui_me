c8ced6aab51bdc0ed483c3add3fd26a500d40679
Mayel de Borniol <git@mayel.space>
2024-02-10 20:29:35 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/847

--- lib/components/profile/links/profile_link_live.ex
+++ lib/components/profile/links/profile_link_live.ex
@@ -10,4 +10,22 @@ defmodule Bonfire.UI.Me.ProfileLinkLive do
   def display_url("https://" <> url), do: url
   def display_url("http://" <> url), do: url
   def display_url(url), do: url
+
+  def render(%{icon: nil, href: href} = assigns) do
+    assigns
+    |> assign(
+      :icon,
+      (URI.parse(href).host || "")
+      |> String.replace("www.", "")
+      |> debug("hooost")
+      |> maybe_icon()
+    )
+    |> render_sface()
+  end
+
+  def maybe_icon("youtu" <> _), do: "ri:youtube-fill"
+  def maybe_icon("orcid" <> _), do: "simple-icons:orcid"
+  def maybe_icon("git" <> _), do: "mdi:git"
+  def maybe_icon("instagram" <> _), do: "ri:instagram-line"
+  def maybe_icon(_), do: nil
 end
