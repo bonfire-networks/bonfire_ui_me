0b57239a0a429eb61a25217f5c95bedb36db461e
Mayel de Borniol <git@mayel.space>
2024-02-23 22:43:06 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/851

--- config/config.exs
+++ config/config.exs
@@ -8,8 +8,6 @@ config :needle, :search_path, [:bonfire_me]
 
 config_file = if Mix.env() == :test, do: "config/test.exs", else: "config/config.exs"
 
-config :bonfire, Bonfire.Common.Repo, database: "bonfire_test"
-
 cond do
   File.exists?("../bonfire/#{config_file}") ->
     IO.puts("Load #{config_file} from local clone of bonfire_spark")
