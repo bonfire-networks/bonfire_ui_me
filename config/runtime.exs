703865d627d2c7c069437d8b5f9c92ff423e5f2c
Giacomo Leidi <goodoldpaul@autistici.org>
2024-03-03 17:20:13 +0100

    Load runtime config before running tests.

---
+++ config/runtime.exs
@@ -0,0 +1,5 @@
+import Config
+
+
+IO.puts("Load runtime config...")
+Bonfire.Common.Config.LoadExtensionsConfig.load_configs(Bonfire.Common.RuntimeConfig)
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
-IO.puts("Prepare to run tests...")
-
+IO.puts("Start app...")
 Application.ensure_all_started(:bonfire)
 
+IO.puts("Prepare to run tests...")
 Code.ensure_loaded!(Bonfire.Testing)
 Bonfire.Testing.configure_start_test()
