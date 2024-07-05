8ae01e6fda638f44675a247211911cc02a44bd38
Mayel de Borniol <git@mayel.space>
2024-07-05 21:37:16 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/869

--- lib/components/settings/preferences/extension_settings_live.ex
+++ lib/components/settings/preferences/extension_settings_live.ex
@@ -2,15 +2,15 @@ defmodule Bonfire.UI.Me.ExtensionSettingsLive do
   use Bonfire.UI.Common.Web, :stateless_component
   import Bonfire.Common.Extensions
 
-  prop extension, :any
+  prop extension, :any, required: true
   prop scope, :atom, default: nil
   prop dep, :map, default: nil
 
-  def render(assigns) do
+  def render(%{extension: extension} = assigns) do
     scoped = Bonfire.Common.Settings.LiveHandler.scoped(assigns[:scope], assigns[:__context__])
     all_extensions = Bonfire.Common.Extensions.all_deps()
 
-    dep = find_dep_by_app_name(all_extensions, assigns[:extension])
+    dep = find_dep_by_app_name(all_extensions, extension)
 
     if assigns[:scope] == :instance and
          Bonfire.Boundaries.can?(assigns[:__context__], :configure, :instance) != true do
@@ -19,7 +19,7 @@ defmodule Bonfire.UI.Me.ExtensionSettingsLive do
       assigns
       |> assign(
         page_title: "Extension",
-        dep: dep || %{},
+        dep: Map.put(dep || %{}, :extra, Bonfire.Common.ExtensionModule.extension(extension)),
         scoped: scoped
       )
       |> render_sface()
