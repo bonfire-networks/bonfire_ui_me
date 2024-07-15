e6e60ab7c079472403f3eeccdfff2fa75e6cef0a
Mayel de Borniol <git@mayel.space>
2024-07-15 17:05:14 +0100

    Bonfire.Me.Archive.LiveHandler

---
+++ lib/live_handlers/archive_live_handler.ex
@@ -0,0 +1,35 @@
+defmodule Bonfire.Me.Archive.LiveHandler do
+  use Bonfire.UI.Common.Web, :live_handler
+  # import Bonfire.Boundaries.Integration
+
+  def handle_event("prepare_archive", _params, socket) do
+    {:ok, task_pid} =
+      Bonfire.UI.Me.ExportController.trigger_prepare_archive_async(socket.assigns[:__context__])
+
+    Process.send_after(self(), :clear_flash, 3000)
+
+    {:noreply,
+     socket
+     |> assign(:export_task_pid, task_pid)
+     |> assign_flash(
+       :info,
+       l(
+         "Preparing your archive... You will notified here when it is ready to download (you can continue browsing Bonfire but please keep it open)."
+       )
+     )}
+  end
+
+  def handle_event("cancel_archive", _params, socket) do
+    {:noreply,
+     case socket.assigns[:export_task_pid] do
+       nil ->
+         assign_flash(socket, :error, "No export task found")
+
+       task_pid ->
+         Process.exit(task_pid, :kill)
+         # display it was cancelled.
+         assign_flash(socket, :info, "Cancelled")
+     end}
+  end
+
+end
