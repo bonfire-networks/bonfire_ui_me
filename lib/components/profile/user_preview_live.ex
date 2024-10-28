f58fbef7a608ccc8a2c3edb7b81a98c86bb5275f
ivan <ivanminutillo@outlook.it>
2024-10-28 13:11:18 +0100

    ui

--- lib/components/profile/user_preview_live.ex
+++ lib/components/profile/user_preview_live.ex
@@ -4,8 +4,11 @@ defmodule Bonfire.UI.Me.UserPreviewLive do
   # import Bonfire.Common.Media
 
   prop user, :map
+  prop href, :string, default: nil
   prop path_prefix, :string, default: "/@"
   prop go, :string, default: nil
-
+  prop with_banner, :boolean, default: false
+  prop with_summary, :boolean, default: false
+  prop is_local, :boolean, required: false, default: true
   slot actions, required: false
 end
