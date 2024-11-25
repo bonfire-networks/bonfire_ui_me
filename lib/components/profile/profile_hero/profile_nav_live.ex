9a75f6cc8ad806313c2be1c14257b690b751a11a
ivan <ivanminutillo@outlook.it>
2024-11-25 09:37:10 +0100

    ui

--- lib/components/profile/profile_hero/profile_nav_live.ex
+++
@@ -1,7 +0,0 @@
-defmodule Bonfire.UI.Me.ProfileNavLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop user, :map
-  prop selected_tab, :any, default: nil
-  prop path, :string, default: "@"
-end
