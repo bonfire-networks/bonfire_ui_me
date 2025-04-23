5e0162aedcd05154002989ff97f2a8c2536f0295
Mayel de Borniol <git@mayel.space>
2025-04-23 19:45:47 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1144

--- lib/components/widgets/widget_users/widget_admins_live.ex
+++ lib/components/widgets/widget_users/widget_admins_live.ex
@@ -2,4 +2,23 @@ defmodule Bonfire.UI.Me.WidgetAdminsLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop widget_title, :string, default: nil
+  prop show_reset_btn, :boolean, default: true
+
+  def handle_event(
+        "reset",
+        _,
+        socket
+      ) do
+    Bonfire.UI.Me.WidgetUsersLive.list_admins_reset()
+
+    debug("")
+
+    {:noreply,
+     socket
+     #  TODO: how to update them without reloading or making this a stateful component
+     |> assign_flash(
+       :info,
+       l("Admins have been refreshed.") <> l(" You need to reload to see updates, if any.")
+     )}
+  end
 end
--- lib/components/widgets/widget_users/widget_users_live.ex
+++ lib/components/widgets/widget_users/widget_users_live.ex
@@ -6,6 +6,9 @@ defmodule Bonfire.UI.Me.WidgetUsersLive do
   prop with_batch_follow, :boolean, default: true
   prop parent_id, :string, default: nil
 
+  @doc "A call to action"
+  slot action
+
   def render(assigns) do
     assigns
     |> assign(
@@ -35,5 +38,9 @@ defmodule Bonfire.UI.Me.WidgetUsersLive do
 
   defp do_list_admins() do
     repo().maybe_preload(Bonfire.Me.Users.list_admins(), [:character, profile: :icon])
+  end
+
+  def list_admins_reset do
+    Cache.reset(&do_list_admins/0, [])
   end
 end
