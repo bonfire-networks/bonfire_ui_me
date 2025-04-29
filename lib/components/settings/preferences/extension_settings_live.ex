a77019dee3e6474e07ebda4879a4079a89379456
Mayel de Borniol <git@mayel.space>
2025-04-29 13:46:24 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1228

--- lib/components/settings/preferences/extension_settings_live.ex
+++ lib/components/settings/preferences/extension_settings_live.ex
@@ -1,57 +1,54 @@
 defmodule Bonfire.UI.Me.ExtensionSettingsLive do
-  use Bonfire.UI.Common.Web, :stateless_component
+  use Bonfire.UI.Common.Web, :stateful_component
   import Bonfire.Common.Extensions
 
   prop extension, :any, required: true
   prop scope, :atom, default: nil
   prop dep, :map, default: nil
 
-  def render(assigns) do
-    assigns
-    |> prepare()
-    |> render_sface()
+  def update(assigns, socket) do
+    socket =
+      socket
+      |> assign(assigns)
+
+    {:ok,
+     socket
+     |> assign(prepare(assigns(socket)))}
   end
+
+  # def render(assigns) do
+  #   assigns
+  #   |> assign(prepare())
+  #   |> render_sface()
+  # end
 
   def prepare(assigns) do
-    # Access directly from assigns
-    extension = assigns.extension
-    scoped = Bonfire.Common.Settings.LiveHandler.scoped(assigns[:scope], assigns[:__context__])
-    all_extensions = Bonfire.Common.Extensions.all_deps()
+    if socket_connected?(assigns) do
+      # Access directly from assigns
+      extension = assigns.extension
+      scoped = Bonfire.Common.Settings.LiveHandler.scoped(assigns[:scope], assigns[:__context__])
 
-    dep = find_dep_by_app_name(all_extensions, extension)
+      if assigns[:scope] == :instance and
+           Bonfire.Boundaries.can?(assigns[:__context__], :configure, :instance) != true do
+        raise Bonfire.Fail, :unauthorized
+      else
+        all_extensions = Bonfire.Common.Extensions.all_deps()
+        dep = find_dep_by_app_name(all_extensions, extension)
 
-    if assigns[:scope] == :instance and
-         Bonfire.Boundaries.can?(assigns[:__context__], :configure, :instance) != true do
-      raise Bonfire.Fail, :unauthorized
+        [
+          page_title: "Extension",
+          dep: Map.put(dep || %{}, :extra, Bonfire.Common.ExtensionModule.extension(extension)),
+          scoped: scoped
+        ]
+      end
     else
-      assigns
-      |> assign(
-        page_title: "Extension",
-        dep: Map.put(dep || %{}, :extra, Bonfire.Common.ExtensionModule.extension(extension)),
-        scoped: scoped
-      )
+      [
+        page_title: l("Loading..."),
+        dep: %{},
+        scoped: nil
+      ]
     end
   end
-
-  # def render(%{extension: extension} = assigns) do
-  #   scoped = Bonfire.Common.Settings.LiveHandler.scoped(assigns[:scope], assigns[:__context__])
-  #   all_extensions = Bonfire.Common.Extensions.all_deps()
-
-  #   dep = find_dep_by_app_name(all_extensions, extension)
-
-  #   if assigns[:scope] == :instance and
-  #        Bonfire.Boundaries.can?(assigns[:__context__], :configure, :instance) != true do
-  #     raise Bonfire.Fail, :unauthorized
-  #   else
-  #     assigns
-  #     |> assign(
-  #       page_title: "Extension",
-  #       dep: Map.put(dep || %{}, :extra, Bonfire.Common.ExtensionModule.extension(extension)),
-  #       scoped: scoped
-  #     )
-  #     |> render_sface()
-  #   end
-  # end
 
   def find_dep_by_app_name(deps, app_name) do
     Enum.find(deps, fn %{app: app} -> app == app_name end)
