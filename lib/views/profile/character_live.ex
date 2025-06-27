9abcb635accaf78b68f7d3632163d3ec3c6d21aa
Mayel de Borniol <git@mayel.space>
2025-06-27 08:22:47 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1383

--- lib/views/profile/character_live.ex
+++ lib/views/profile/character_live.ex
@@ -21,31 +21,33 @@ defmodule Bonfire.UI.Me.CharacterLive do
     current_user = current_user(socket)
     current_username = e(current_user, :character, :username, nil)
 
-    {path, user_etc} =
-      case Map.get(params, "username") || Map.get(params, "id") do
+    {prefix, user_etc, username} =
+      case maybe_sanitize_path(Map.get(params, "username")) || Map.get(params, "id") do
         nil ->
-          {"@", current_user}
+          {"@", current_user, current_username}
 
         username when username == current_username ->
-          {"@", current_user}
+          {"@", current_user, current_username}
 
         "@" <> username ->
-          {"@", "/@" <> username}
+          {"@", "/@" <> username, username}
 
         "+" <> username ->
-          {"+", "/+" <> username}
+          {"+", "/+" <> username, username}
 
         "&" <> username ->
-          {"&", "/&" <> username}
+          {"&", "/&" <> username, username}
 
         username ->
           # TODO: really need to query here?
-          {"@", LiveHandler.get(username)}
+          {nil, LiveHandler.get(username), username}
       end
       |> debug("user_etc")
 
     if user_etc do
-      if is_binary(user_etc) do
+      current_url = current_url(socket) || maybe_get(URI.parse(url), :path)
+
+      if is_binary(user_etc) and current_url != user_etc do
         debug("redir to correct profile path")
 
         {:noreply,
@@ -54,34 +56,35 @@ defmodule Bonfire.UI.Me.CharacterLive do
            user_etc
          )}
       else
+        user_etc = if is_binary(user_etc), do: LiveHandler.get(username), else: user_etc
+
         if not is_nil(id(current_user)) or Integration.is_local?(user_etc) or
              id(user_etc) ==
                maybe_apply(Bonfire.Federate.ActivityPub.AdapterUtils, :service_character_id, []) do
           debug("show profile locally")
 
-          redirect_to_path =
-            case path(user_etc) do
+          maybe_redirect_to_path =
+            case path(user_etc) |> maybe_sanitize_path() do
               "/discussion/" <> _ ->
                 false
 
               "/character/" <> _ ->
                 false
 
-              "/%26" <> username ->
+              "/&" <> username ->
+                # workaround for groups bug
                 module_enabled?(Bonfire.UI.Groups, current_user) && "/group/" <> username
 
-              path ->
-                path
+              maybe_redirect_to_path ->
+                maybe_redirect_to_path
             end
             |> debug(url)
 
-          current_url = current_url(socket) || maybe_get(URI.parse(url), :path)
-
-          if redirect_to_path && current_url != redirect_to_path do
+          if is_binary(maybe_redirect_to_path) and current_url != maybe_redirect_to_path do
             {:noreply,
              redirect_to(
                socket,
-               redirect_to_path
+               maybe_redirect_to_path
              )}
           else
             debug("show a simple fallback profile")
@@ -97,7 +100,7 @@ defmodule Bonfire.UI.Me.CharacterLive do
              |> assign(LiveHandler.user_assigns(user_etc, current_user))
              |> assign(
                character_type: :unknown,
-               path: path
+               path: prefix
              )}
           end
         else
@@ -118,4 +121,10 @@ defmodule Bonfire.UI.Me.CharacterLive do
        |> redirect_to(path(:error, :not_found))}
     end
   end
+
+  defp maybe_sanitize_path(path) when is_binary(path) do
+    URI.decode(path)
+  end
+
+  defp maybe_sanitize_path(_), do: nil
 end
