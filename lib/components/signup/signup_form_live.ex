d7f3838dc048768b6e11db9ac29e24336edd7090
Mayel de Borniol <git@mayel.space>
2025-06-27 20:40:00 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1396

--- lib/components/signup/signup_form_live.ex
+++ lib/components/signup/signup_form_live.ex
@@ -5,4 +5,24 @@ defmodule Bonfire.UI.Me.SignupFormLive do
   prop error, :any
   prop invite, :any
   prop auth_second_factor_secret, :any, default: nil
+
+  defp prepare_error(e) when is_binary(e) do
+    e
+  end
+
+  defp prepare_error({e, _}) when is_binary(e) do
+    e
+  end
+
+  defp prepare_error({:error, e}) when is_binary(e) do
+    e
+  end
+
+  defp prepare_error(nil) do
+    nil
+  end
+
+  defp prepare_error(e) do
+    inspect(e)
+  end
 end
