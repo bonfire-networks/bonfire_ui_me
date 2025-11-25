44e1695953fd5600c587dfd9db8bd0cc76eb4eee
ivan <ivanminutillo@outlook.it>
2025-11-25 12:03:43 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/916

--- lib/api/masto_account_controller.ex
+++ lib/api/masto_account_controller.ex
@@ -14,5 +14,14 @@ if Application.compile_env(:bonfire_api_graphql, :modularity) != :disabled do
     def verify_credentials(conn, params), do: Adapter.me(params, conn)
 
     def show_preferences(conn, params), do: Adapter.get_preferences(params, conn)
+
+    # Mutes and Blocks endpoints
+    def mutes(conn, params), do: Adapter.mutes(params, conn)
+    def blocks(conn, params), do: Adapter.blocks(params, conn)
+
+    def mute(conn, %{"id" => id}), do: Adapter.mute_account(%{"id" => id}, conn)
+    def unmute(conn, %{"id" => id}), do: Adapter.unmute_account(%{"id" => id}, conn)
+    def block(conn, %{"id" => id}), do: Adapter.block_account(%{"id" => id}, conn)
+    def unblock(conn, %{"id" => id}), do: Adapter.unblock_account(%{"id" => id}, conn)
   end
 end
