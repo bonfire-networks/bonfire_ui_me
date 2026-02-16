c2f53e89727650f4b0c8f9e5e371d08fcc4682d4
ivan <ivanminutillo@outlook.it>
2026-02-16 11:15:00 +0100

    ui

---
+++ lib/components/settings/safety/safety_live.ex
@@ -0,0 +1,20 @@
+defmodule Bonfire.UI.Me.SettingsViewsLive.SafetyLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop selected_tab, :any
+  prop scope, :atom, default: nil
+
+  def render(assigns) do
+    scoped = Bonfire.Common.Settings.LiveHandler.scoped(assigns[:scope], assigns[:__context__])
+
+    if assigns[:scope] == :instance and
+         Bonfire.Boundaries.can?(assigns[:__context__], :configure, :instance) != true do
+      raise Bonfire.Fail, :unauthorized
+    else
+      assigns
+      |> assign(scoped: scoped)
+      |> assign(page_title: l("Safety"))
+      |> render_sface()
+    end
+  end
+end
