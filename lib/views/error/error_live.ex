06d515a3774a52eaadb1c5ef8856bdd41fdb6fd3
ivan <ivanminutillo@outlook.it>
2026-05-20 12:00:54 +0200

    error ux

--- lib/views/error/error_live.ex
+++ lib/views/error/error_live.ex
@@ -4,17 +4,23 @@ defmodule Bonfire.UI.Me.ErrorLive do
   on_mount {LivePlugs, [Bonfire.UI.Me.LivePlugs.LoadCurrentUser]}
 
   def mount(params, _session, socket) do
+    code = params["code"]
+    flash_reason = Phoenix.Flash.get(socket.assigns[:flash] || %{}, :error)
+
+    headline =
+      maybe_apply(Bonfire.Fail, :get_error_msg, code, fn -> nil end) ||
+        flash_reason ||
+        default_msg()
+
     {
       :ok,
       socket
       |> assign_new(:page, fn -> nil end)
-      |> assign_new(:code, fn -> params["code"] end)
-      |> assign_new(:message, fn ->
-        maybe_apply(Bonfire.Fail, :get_error_msg, params["code"], &default_msg/0) || default_msg()
-      end)
-      #  |> assign_new(:without_sidebar, fn -> true end)
+      |> assign_new(:code, fn -> code end)
+      |> assign_new(:message, fn -> headline end)
+      |> assign_new(:page_title, fn -> headline end)
     }
   end
 
-  defp default_msg, do: l("There was an error.")
+  defp default_msg, do: l("Something didn't load")
 end
